5920bd939e19b9821616e0b87342b11cef8b75c6
Amanda Grau <amandarengrau@gmail.com>
2024-02-26 16:34:50 -0600

    Display player greeting

--- game.py
+++ game.py
@@ -1,5 +1,10 @@
 """A number-guessing game."""
+
 # Computer chooses a random number between 1-100 
 # Ask the user to guess the number
 # Once user guesses, tell user if their guess is too low or too high
 # Game is over once the user guesses the random number
+
+user_name = input("Hello there! Please enter your name: ")
+print(f"Welcome, {user_name}!")
+print("I'm thining of a number between 1 and 100. Can you guess what the number is?")
